d2185a85b856029d8d07d7d066b4cbe61e33a80b
Alevdelgado <110568267+Alevdelgado@users.noreply.github.com>
2024-04-09 17:37:10 -0600

    added encuestado

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import psycopg2
 
 class Database:
     def __init__(
-        self, database="db_name", host="db_host", user="db_user", password="db_pass", port="db_port"
+        self, database="db_name", host="db_host", user="db_user", password="db_pass", port="db_port", respondents = "db_respondents"
     ):
         self.conn = psycopg2.connect(
-            database=database, host=host, user=user, password=password, port=port
+            database=database, host=host, user=user, password=password, port=port, respondents=respondents
         )
 
     def register(self, user):
@@ -59,3 +59,47 @@ class Database:
         self.conn.commit()
         cursor.close()
         return user_deleted
+
+# ENCUESTADOS
+
+    def get_respondents(self):
+        cursor = self.conn.cursor()
+        query = "SELECT id, id_usuario, nombre, pass, edad FROM encuestado;"
+        cursor.execute(query)
+        result = cursor.fetchall()#Lo que retorno la funcion de postgres
+        cursor.close()
+        return result
+    
+    def get_respondents_by_id(self, respondents_id):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM encuestado WHERE id = {respondents_id};")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+    
+    def register_respondents(self, respondents):
+        cursor = self.conn.cursor()
+        query = "SELECT register_respondents(%s, %s, %s);" #nombre, pass, edad
+        parametros = (respondents['nombre'],respondents['password'], respondents['edad'])
+        cursor.execute(query, parametros)
+        response = cursor.fetchall()
+        cursor.close()
+        return response
+
+    def update_respondents(self, request_respondents):
+        cursor = self.conn.cursor()
+        query_respondents = "SELECT update_respondents(%(id)s,%(nombre)s,%(password)s,%(edad)s);"
+        cursor.execute(query_respondents, request_respondents)  
+        self.conn.commit()
+        cursor.close()
+        return request_respondents
+
+    def delete_respondents(self, request_respondents_id):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM Encuestado WHERE id = {request_respondents_id};")
+        respondents_deleted = cursor.fetchall()#Lo que retorno la funcion de postgres
+        cursor.execute(f"DELETE FROM Encuestado WHERE id = {request_respondents_id};")
+        self.conn.commit()
+        cursor.close()
+        return respondents_deleted
+    
