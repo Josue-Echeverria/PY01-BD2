2c0559423c6a90d64c3bec5b30a6c2238de2c0cb
Alevdelgado <110568267+Alevdelgado@users.noreply.github.com>
2024-04-12 19:05:57 -0600

    app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_RESPONDENTS = os.getenv("DB_RESPONDENTS")
 
 
-db = Database(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASSWORD, port=DB_PORT, respondents=DB_RESPONDENTS)
+
+db = Database(database=DB_NAME, host=DB_HOST, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
 
 app = Flask(__name__)
 appService = AppService(db)
